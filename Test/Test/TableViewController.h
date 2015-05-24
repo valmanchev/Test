a37c584d00f4ef8b8274ebc5ef9b1e4b67d20474
valmanchev <vmanchev@yahoo.com>
2015-05-24 14:54:42 +0300

    added new class

---
+++ Test/Test/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Test
+//
+//  Created by Valeri Manchev on 5/24/15.
+//  Copyright (c) 2015 Valeri Manchev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
